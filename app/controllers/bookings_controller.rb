afcd6008c77ca89a7fda51a9531e51b491e66c95
seeleng <seelengxd@gmail.com>
2021-12-18 03:22:48 +0800

    Finally got nested attributes to work

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,27 @@
 class BookingsController < ApplicationController
   def new
-    @flight_id = params[:flight_id]
-    @number_of_passengers = params[:number_of_passengers]
+    @flight = Flight.find(params[:flight_id])
+    @number_of_passengers = params[:number_of_passengers].to_i
+    @booking = Booking.new(flight_id: params[:flight_id])
+    @number_of_passengers.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(permitted_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @flight = @booking.flight
+    @passengers = @booking.passengers
+  end
+
+  def permitted_params
+    params.require(:booking).permit(:flight_id, :passenger, passengers_attributes: [:name, :email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flights, only: [:index]
-  resources :bookings, only: [:new]
+  resources :bookings, only: [:new, :create, :show]
   root 'flights#index'
 end
